73aaae117f81bd6198a0aa0aa641f51844301817
RaphSch <raph.rjfs@hotmail.fr>
2018-05-14 15:16:32 +0200

    Corrected stupid mistake

--- run_biggiesmalls.R
+++ run_biggiesmalls.R
@@ -11,25 +11,27 @@ source("biggiesmalls.R")
 # Take arguments in
 arguments = as.numeric(commandArgs(TRUE))
 
-# Parameter that can vary
-islandSizes = c(100,200,400)
-competitiveAdvantages = c(0.01,0.1,1)
-resourceScalings = c(-0.01,-0.1,-1)
-bodySizes0 = c(10, 1000000)
-replicates = c(1,2,3)
 
-# Pick parameter values to run
-parameters = NULL
-parameters[1] = islandSizes[arguments[1]] #island size
-parameters[2] = competitiveAdvantages[arguments[2]] #competitive advantage
-parameters[3] = resourceScalings[arguments[3]] #resource scaling
-parameters[4] = bodySizes0[arguments[4]] #initial mean body size
-parameters[5] = 0.05 #relative initial standard deviation in body size
-parameters[6] = 10 #initial population size
-parameters[7] = 25000 #simulation time
-parameters[8] = 0.05 #mutational standard deviation
-parameters[9] = 0.01 #mutation rate
-parameters[10] = replicates[arguments[5]] #replicate
+# Create all combinations of parameters to run
+allParameters = expand.grid(
+  c(100,200,400), #island size
+  c(0.01,0.1,1), #competitive advantage
+  c(-0.01,-0.1,-1), #resource scaling
+  c(10,1000000), #initial body size
+  0.05, #standard deviation of initial body size
+  10, #initial population size
+  25000, #simulation time
+  0.05, #mutational standard deviation
+  0.01, #mutation rate
+  c(1,2,3) #replicates
+)
+
+# Or arrange your own custom table of parameters to run
+# It has to have 10 columns
+
+# Pick the right combination to run
+parameters = allParameters[arguments[1],]
+
 
 # Run the model
 biggiesmalls(parameters)
